0786df737bf6591a493c34e3d524024b24d0c1a1
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-09-10 18:09:07 +0530

    Added Loading component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.scss';
+</script>
+
+<nav>
+	<div />
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/projects">Projects</a></li>
+		<li><a href="/about">About</a></li>
+	</ul>
+</nav>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<!-- <Loading /> -->
+<h1 class="title">My content</h1>
+
+<style>
+	.title {
+		font-size: 5rem;
+	}
+</style>
