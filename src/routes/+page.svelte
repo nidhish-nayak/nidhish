b4f6709965cd4ae27b4dcab6dcac1db0c206f725
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-09-12 19:37:27 +0530

    Added first GSAP line check

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		gsap.from('.box', { x: -300, duration: 0.5 });
+	});
+</script>
+
+<h1 class="box">Hey there</h1>
